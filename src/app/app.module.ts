a1aee0d07459d9c8a58bac5c467d7e1e062416cd
Lydia Gregory <ljg2gb@virginia.edu>
2020-11-20 14:06:34 -0500

    adds components and styles header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ListComponent } from './shopping-list/list/list.component';
+import { EditListComponent } from './shopping-list/edit-list/edit-list.component';
+import { RecipesComponent } from './recipe-book/recipes/recipes.component';
+import { RecipeComponent } from './recipe-book/recipes/recipe/recipe.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ListComponent,
+    EditListComponent,
+    RecipesComponent,
+    RecipeComponent,
+    RecipeDetailsComponent,
   ],
   imports: [
     BrowserModule
