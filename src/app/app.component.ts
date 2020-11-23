ff071d35d9d5e7c12d66abbfdd7aaf70fdf96422
Lydia Gregory <ljg2gb@virginia.edu>
2020-11-23 11:11:39 -0500

    adds custom event and property binding make navigation useable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+  recipesSelected: boolean = true
+
+  onNavigation(isRecipe: boolean) {
+    if (isRecipe === true) {
+      this.recipesSelected = true
+    } else {
+      this.recipesSelected = false
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header-component',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() navigatePages= new EventEmitter<boolean>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectRecipes() {
+    this.navigatePages.emit(true)
+  }
+  
+  selectShoppingList() {
+    this.navigatePages.emit(false)
+  }
 }
